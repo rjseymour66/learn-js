9d61e53cd8758cb45c1383f1a6e7533d529abe07
RJS66 <rjs.github.com>
2020-04-17 22:12:08 -0400

    dom todo list

---
+++ scripts/window-size-DOM.js
@@ -0,0 +1,6 @@
+const div = document.querySelector('div');
+let winWidth = window.innerWidth;
+let winHeight = window.innerHeight;
+
+div.style.width = winWidth + 'px';
+div.style.height = winHeight + 'px';
